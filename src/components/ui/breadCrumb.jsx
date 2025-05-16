c73978bbc43b3ac8de966e090718576b43799bd7
vinayak-vayuz <vinayaktripathi@vayuz.com>
2025-05-16 15:36:10 +0530

    ui changes

--- src/components/ui/breadCrumb.jsx
+++ src/components/ui/breadCrumb.jsx
@@ -1,40 +1,43 @@
-import { Breadcrumbs, Typography } from "@mui/material";
+import { Box, Breadcrumbs, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 
 export const BreadCrumbCustom = ({ links = [], pageTitle }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const navigateGlobal = window.navigateGlobal
+  const navigateGlobal = window.navigateGlobal;
   return (
     <>
-      {/* <Box> */}
-      <Breadcrumbs aria-label="breadcrumb">
-        {links.map(({ label, to, global }, index) => (
-          <>
-            <button onClick={() => {
-              console.log(global, to, 'wowow');
-              if (global) {
-                navigateGlobal(to)
-              } else {
-                navigate(to)
-              }
-            }
-            }>{label}</button>
-            {/* <Link
+      <Box>
+        <Breadcrumbs aria-label="breadcrumb">
+          {links.map(({ label, to, global }, index) => (
+            <>
+              <button
+                onClick={() => {
+                  console.log(global, to, "wowow");
+                  if (global) {
+                    navigateGlobal(to);
+                  } else {
+                    navigate(to);
+                  }
+                }}
+              >
+                {label}
+              </button>
+              {/* <Link
             key={index}
             to={to}
             style={{ textDecoration: "none", color: "inherit" }}
             >
             {label}
             </Link> */}
-          </>
-        ))}
-        <Typography sx={{ color: "text.primary" }}>{pageTitle}</Typography>
-      </Breadcrumbs>
-      <Typography fontSize={24} sx={{ color: "text.primary" }}>
-        {pageTitle}
-      </Typography>
-      {/* </Box> */}
+            </>
+          ))}
+          <Typography sx={{ color: "text.primary" }}>{pageTitle}</Typography>
+        </Breadcrumbs>
+        <Typography fontSize={24} sx={{ color: "text.primary" }}>
+          {pageTitle}
+        </Typography>
+      </Box>
     </>
   );
 };
